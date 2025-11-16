7f02341eacb69965094abba1fc5691f6035f0e9c
Raul Battistini <raulbattistini.3@gmail.com>
2025-11-16 13:57:21 -0300

    feat: sample creation of server and client

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"go-rpc/configs"
+	"go-rpc/internal/coordinator"
+	"go-rpc/internal/server"
 	"go-rpc/internal/util"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
+
+var NetServerAddress int
+var NetServerHost string
 
 func main() {
 	serverCfg, _ := configs.LoadConfig("server-config.yaml", "server")
+	parsedCfgServer := serverCfg.(configs.ServerConfig)
+	NetServerAddress = parsedCfgServer.Address
+	NetServerHost = parsedCfgServer.Host
 
 	util.Log().Info("Configs for server loaded: %v", serverCfg)
+
+	coordTimeout := time.Duration(parsedCfgServer.SessionTimeoutSeconds) * time.Second
+
+	coord := coordinator.NewCoordinator(coordTimeout)
+	defer coord.Stop()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		util.Log().Info("Shutting down RPC Server")
+		coord.Stop()
+		os.Exit(0)
+	}()
+
+	util.Log().Info("Starting lock coordinator server...")
+	if err := server.InitRPCServer(parsedCfgServer.Address, parsedCfgServer.Host, coord); err != nil {
+		util.Log().Fatal("Failed to start RPC server: %v", err)
+	}
 }
