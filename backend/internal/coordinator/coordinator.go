6695618e760925574e3236f1d530b2b84d0c8f0a
Raul Battistini <raulbattistini.3@gmail.com>
2025-11-16 13:56:42 -0300

    feat: coordinator methods completion

--- backend/internal/coordinator/coordinator.go
+++ backend/internal/coordinator/coordinator.go
@@ -3,11 +3,13 @@
 package coordinator
 
 import (
+	"fmt"
 	"go-rpc/internal/util"
 	"net/rpc"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"go.bryk.io/pkg/ulid"
 )
 
@@ -22,7 +24,7 @@ type Session struct {
 
 type Lock struct {
 	Name      string
-	Owner     string
+	Owner     ulid.ULID
 	Token     string
 	ExpiresAt time.Time
 	Timer     *time.Timer
@@ -33,6 +35,7 @@ type Coordinator struct {
 	locks          map[string]*Lock
 	sessions       map[ulid.ULID]*Session
 	sessionTimeout time.Duration
+	sessionAfw     time.Duration
 	stopChan       chan struct{}
 }
 
@@ -43,7 +46,7 @@ func NewCoordinator(sessionTimeout time.Duration) *Coordinator {
 		sessionTimeout: sessionTimeout,
 		stopChan:       make(chan struct{}),
 	}
-	// go c.cleanupAfws()
+	go c.cleanupAfws()
 	return c
 }
 
@@ -85,4 +88,238 @@ func (c *Coordinator) Heartbeat(sessionID ulid.ULID) (bool, time.Time) {
 	return true, expiresAt
 }
 
-func (c *Coordinator) AcquireLock(req AcquireLockRequest, resp AcquireLockResponse)
+func (c *Coordinator) AcquireLock(req AcquireLockRequest) AcquireLockResponse {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	session, exists := c.sessions[req.SessionID]
+	if !exists {
+		return AcquireLockResponse{
+			Resp: GenericResponse{
+				Success: false,
+				Message: "This session wasn't found",
+			},
+		}
+	}
+
+	lock, lockExists := c.locks[req.LockName]
+	if lockExists {
+		if lock.Owner == req.SessionID {
+			return AcquireLockResponse{
+				Resp: GenericResponse{
+					Success: true,
+					Message: "Lock already held by the client",
+				},
+				Token:     lock.Token,
+				ExpiresAt: lock.ExpiresAt, // shouldnt the expires at be redefined here with a time add approach starting from now
+			}
+		}
+		if req.WaitTime > 0 { // time for the lock to be released by its owner before getting accessed
+			// should do a Futex.wake for this call for the 'sleeping' service to access this right after the lock is released by the first session
+			return AcquireLockResponse{
+				Resp: GenericResponse{
+					Success: false,
+					Message: fmt.Sprintf("Lock currently held by another client. Wait %T before trying again", req.WaitTime),
+				},
+			}
+		}
+
+		return AcquireLockResponse{
+			Resp: GenericResponse{
+				Success: false,
+				Message: "Lock being currently held",
+			},
+		}
+
+	}
+
+	token := uuid.New().String()
+	expiresAt := time.Now().Add(req.Timeout) // do the request parse with criteria - ofc timeout <= max allowed
+
+	timer := time.AfterFunc(req.Timeout, func() {
+		c.expireLock(req.LockName, token)
+	})
+
+	c.locks[req.LockName] = &Lock{
+		Name:      req.LockName,
+		Owner:     req.SessionID,
+		Token:     token,
+		ExpiresAt: expiresAt,
+		Timer:     timer,
+	}
+
+	session.HeldLocks = append(session.HeldLocks, req.LockName)
+
+	util.Log().Info("Lock acquired: %s by session %v, expiring at %T")
+
+	return AcquireLockResponse{
+		Resp: GenericResponse{
+			Success: true,
+			Message: "Lock acquired successfully",
+		},
+		Token:     token,
+		ExpiresAt: expiresAt,
+	}
+}
+
+func (c *Coordinator) ReleaseLock(req ReleaseLockRequest) ReleaseLockResponse {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	lock, exists := c.locks[req.LockName]
+	if !exists {
+		return ReleaseLockResponse{
+			Resp: GenericResponse{
+				Success: false,
+				Message: "Lock does not exists",
+			},
+		}
+	}
+
+	if lock.Owner != req.SessionID {
+		return ReleaseLockResponse{
+			Resp: GenericResponse{
+				Success: false,
+				Message: "The client does not hold the lock",
+			},
+		}
+	}
+
+	if lock.Token != req.Token {
+		return ReleaseLockResponse{
+			Resp: GenericResponse{
+				Success: false,
+				Message: "Invalid token",
+			},
+		}
+	}
+
+	lock.Timer.Stop()
+	delete(c.locks, req.LockName)
+	util.Log().Debug("Deleted the lock %s at %T", lock.Name, time.Now())
+
+	if session, exists := c.sessions[req.SessionID]; exists {
+		session.HeldLocks = util.RemoveStr(session.HeldLocks, lock.Name)
+	}
+	util.Log().Debug("Lock %s released by session %v", req.LockName, req.SessionID)
+
+	return ReleaseLockResponse{
+		Resp: GenericResponse{
+			Success: true,
+			Message: "Lock released succesfully",
+		},
+	}
+}
+
+func (c *Coordinator) expireLock(lockName, token string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	lock, exists := c.locks[lockName]
+	if !exists {
+		util.Log().Debug("Lock %s was already released", lockName)
+		return
+	}
+	if lock.Token != token {
+		util.Log().Debug("Provided token %s was different from lock's %s", token, lock.Token)
+		return
+	}
+
+	util.Log().Info("Lock expired: %s was held by session %v", lockName, lock.Owner)
+
+	if session, exists := c.sessions[lock.Owner]; exists {
+		session.HeldLocks = util.RemoveStr(session.HeldLocks, lockName)
+	}
+
+	delete(c.locks, lockName)
+}
+
+func (c *Coordinator) cleanupAfws() {
+	ticker := time.NewTicker(c.sessionAfw)
+	defer ticker.Stop()
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// looks like a gc stop the world approach running endlessly
+
+	for {
+		select {
+		case <-ticker.C:
+			c.sessionCleanupProc()
+		case <-c.stopChan:
+			// application graceful shutdown
+			return
+		}
+	}
+}
+
+func (c *Coordinator) sessionCleanupProc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	now := time.Now()
+	staleSessionIds := make([]ulid.ULID, 0)
+
+	for sessionID, session := range c.sessions {
+		if now.Sub(session.LastSeen) > c.sessionTimeout {
+			staleSessionIds = append(staleSessionIds, sessionID)
+		}
+	}
+
+	for _, sessionID := range staleSessionIds {
+		session := c.sessions[sessionID]
+
+		util.Log().Info("Cleaning up afw session %v", sessionID)
+
+		for _, lockName := range session.HeldLocks {
+			if lock, exists := c.locks[lockName]; exists {
+				lock.Timer.Stop() // as to avoid side effects on later phases
+				delete(c.locks, lockName)
+				util.Log().Info("Auto released lock %s from session %v (afw)", lockName, sessionID)
+			}
+		}
+
+		delete(c.sessions, sessionID)
+	}
+}
+
+func (c *Coordinator) clearSessions() {
+	if len(c.sessions) > 0 {
+		for _, session := range c.sessions {
+			delete(c.sessions, session.ID)
+		}
+	}
+}
+
+func (c *Coordinator) clearLocks() {
+	if len(c.locks) > 0 {
+		for _, lock := range c.locks {
+			delete(c.locks, lock.Name)
+		}
+	}
+}
+
+func (c *Coordinator) Stop() {
+	close(c.stopChan)
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for _, lock := range c.locks {
+		// nested, can be improved
+		lock.Timer.Stop()
+		// also avoids side effects
+		c.clearLocks()
+		c.clearSessions()
+	}
+}
+
+func (c *Coordinator) GetStats() StatsServer {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return StatsServer{
+		ActiveSessions: len(c.sessions),
+		ActiveLocks:    len(c.locks),
+	}
+}
--- backend/internal/coordinator/types.go
+++ backend/internal/coordinator/types.go
@@ -4,7 +4,10 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"go.bryk.io/pkg/ulid"
 )
+
+// next phase: return granted/denied/grant-expired in the response instead of a bool
 
 type GenericResponse struct {
 	Success bool
@@ -13,21 +16,21 @@ type GenericResponse struct {
 
 type AcquireLockRequest struct {
 	LockName  string
-	SessionID string
+	SessionID ulid.ULID // later on it will be useful to order the sessions time-wise
 	Timeout   time.Duration
 	WaitTime  time.Duration
 }
 
 type AcquireLockResponse struct {
 	Resp      GenericResponse
-	Token     uuid.UUID
+	Token     string // meant to work like an api key
 	ExpiresAt time.Time
 }
 
 type ReleaseLockRequest struct {
-	LockName  string
-	Token     uuid.UUID
-	SessionID string
+	LockName  string // despite using uuids in this field, keeping some flexibility as for this phase
+	Token     string
+	SessionID ulid.ULID
 }
 
 type ReleaseLockResponse struct {
@@ -35,12 +38,13 @@ type ReleaseLockResponse struct {
 }
 
 type HeartbeatRequest struct {
-	SessionID string
+	SessionID ulid.ULID
 	Token     uuid.UUID
 }
 
 type HeartbeatResponse struct {
-	Resp GenericResponse
+	Resp      GenericResponse
+	ExpiresAt time.Time
 }
 
 type RegisterSessionRequest struct {
@@ -48,6 +52,11 @@ type RegisterSessionRequest struct {
 }
 
 type RegisterSessionResponse struct {
-	SessionID string
+	SessionID ulid.ULID
 	ExpiresAt time.Time
+}
+
+type StatsServer struct {
+	ActiveSessions int
+	ActiveLocks    int
 }
